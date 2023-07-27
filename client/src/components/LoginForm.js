3b8f3175c11e35a5cafaf36d1a09a831961fc737
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-07-27 19:56:36 -0400

    user login LoginForm

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -41,16 +41,9 @@ const LoginForm = () => {
         variables: { ...userFormData },
       });
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
     }
 
     setUserFormData({
