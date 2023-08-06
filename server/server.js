1b1e2612bcc705b28792fea241ab6efb29996c15
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-08-06 09:35:22 -0400

    modified server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
+const routes = require("./routes");
 
-// add apollo server
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
@@ -10,7 +10,6 @@ const { authMiddleware } = require("./utils/auth");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// add apollo middleware
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
@@ -31,6 +30,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
